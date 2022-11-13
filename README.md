ee4cd60c8bb10f2dc2854a3fe463c4952dbbac4c
Harri Virtanen <harri@niodi.fi>
2022-11-13 15:54:32 +0200

    Upgrade all

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # @renodesu/stylelint-config
+
+Installation
+
+```
+npm i -D git@github.com:renodesu/stylelint-config.git
+```
+
+Add it to project .eslintrc.js:
+
+```js
+module.exports = {
+  extends: '@renodesu/stylelint-config',
+}
+```
--- index.js
+++ index.js
@@ -4,4 +4,20 @@ module.exports = {
     'stylelint-config-prettier-scss',
     'stylelint-config-idiomatic-order',
   ],
+  rules: {
+    // Tailwind @rules
+    'at-rule-no-unknown': null,
+    'scss/at-rule-no-unknown': [
+      true,
+      {
+        ignoreAtRules: [
+          'tailwind',
+          'apply',
+          'variants',
+          'responsive',
+          'screen',
+        ],
+      },
+    ],
+  },
 }
